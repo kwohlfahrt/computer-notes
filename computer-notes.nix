3683425a5f51738388b132e11932ac3209377fb3
Kai Wohlfahrt <kai.wohlfahrt@gmail.com>
2021-03-22 20:25:26 +0000

    Use asciidoctor-multipage
    Generally OK, but can't work out how to display >1 level per page.

--- computer-notes.nix
+++ computer-notes.nix
@@ -1,14 +1,14 @@
-{ stdenv, asciidoctor }:
+{ stdenv, asciidoctor, asciidoctor-multipage }:
 
 stdenv.mkDerivation {
   name = "computer-notes";
   version = "0.1.0";
 
   phases = [ "unpackPhase" "buildPhase" ];
-  buildInputs = [ asciidoctor ];
+  buildInputs = [ asciidoctor-multipage ];
 
   src = ./src;
   buildPhase = ''
-    asciidoctor --failure-level=WARNING -b html5 index.adoc -o $out/index.html
+    asciidoctor --failure-level=WARNING -r asciidoctor-multipage -b multipage_html5 index.adoc -o $out/index.html
   '';
 }
